a797e060f434222ba0c430b1e3aa1d643f161eac
arundhi04101996@gmail.com <arundhi04101996@gmail.com>
2023-08-26 17:08:26 +0530

    homepage-basic-structure-completed

--- src/pages/homepage/Homepage.js
+++ src/pages/homepage/Homepage.js
@@ -1,16 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Homepage.css"
 import Topbar from '../../components/topbar/Topbar'
 import Addnote from '../../components/addnote/Addnote'
 
+
 function Homepage() {
+
+    const [data,setData]=useState([
+        {
+          id:1,
+          title:"abc",
+          content:"lorem dsfh  dsfug jksdf jhdsf jhdsg sdjkg hdg zc "
+        },
+        {
+          id:2,
+          title:"sf",
+          content:"lorem dsfh  dsfug jksdf jhdsf jhdsg sdjkg hdg zc "
+        },
+        {
+          id:3,
+          title:"ggv",
+          content:"lorem dsfh  dsfug jksdf jhdsf jhdsg sdjkg hdg zc "
+        }
+      ])
+
+
   return (
     <div className='homepageWrapper'>
         <div className='homebarTop'>
             <Topbar/>
         </div>
         <div className='homepageMiddle'>
-          <Addnote/>
+          <Addnote data={data}  />
+        </div>
+        <hr/>
+        <div className='homePageBottom'>
+
+          <div className='notesContainer'>
+          {
+          data?.map((item)=>{
+            return(
+              <div  className='notesIndividualContainer'>
+                <div className='notesTop'>
+                  <h5 className='noteTitle'>{item.title}</h5>
+                  <span className='noteDate'>25-08-2023</span>
+                </div>
+                  <hr/>
+                <div className='notesMiddle'>
+                   <p className='noteContent'>{item.content}</p>
+                </div>
+                <hr/>
+                <div className='notesBottom'>
+                    <button className='editButton'>Edit</button>
+                    <button className='deleteButton'>Delete</button>
+                </div>
+              </div>
+            )
+          })
+}
+          </div>
         </div>
     </div>
   )
